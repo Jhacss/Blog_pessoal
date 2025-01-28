85b2ec3e56842727542f3538216240d1cc5601f9
Jhacss <henryki09.joao@gmail.com>
2025-01-28 09:25:39 -0300

    :Sparkles: Deploy do projeto Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,20 +9,16 @@ import { TemasModule } from './temas/temas.module';
 import { AuthModule } from './auth/auth.module';
 import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/prod.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_blogpessoal',
-      entities: [Postagem, Temas, Usuario],
-      synchronize: true,
-    //  logging: true,
-    }),
+    ConfigModule.forRoot(),
+TypeOrmModule.forRootAsync({
+	useClass: ProdService, //DevService para rodar local
+    imports: [ConfigModule],
+}),
     PostagemModule,
     TemasModule,
     AuthModule,
