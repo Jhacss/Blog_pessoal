783cdcc236ec4146f70745773e226105b9304558
Jhacss <henryki09.joao@gmail.com>
2025-03-21 21:23:17 -0300

    :bug:

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,33 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle('Blog Pessoal')
-  .setDescription('Projeto Blog Pessoal')
-  .setContact("João Henrique","https://www.linkedin.com/in/jo%C3%A3o-henrique-0665081a2/","jh09.joao@gmail.com")
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
+    .setTitle('Blog Pessoal')
+    .setDescription('Projeto Blog Pessoal')
+    .setContact(
+      "João Henrique",
+      "https://www.linkedin.com/in/jo%C3%A3o-henrique-0665081a2/",
+      "jh09.joao@gmail.com",
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
 
-  process.env.TZ = '-03.00'; 
+  process.env.TZ = '-03:00';
 
-  //habilitando globalmente  a validação de dados
+  // Habilitando globalmente a validação de dados
   app.useGlobalPipes(new ValidationPipe());
 
-  //habilitando o Cors na aplicação feito para linkar o back com o front sendo hospedados em hosts diferentes
+  // Habilitando CORS na aplicação( habilitar o front-end)
   app.enableCors();
 
-  await app.listen(process.env.PORT ?? 5432);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
